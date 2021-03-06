dfa94eb74c3b83af78896c38de3675d6b4dff31b
roneo.org <roneo+dev@riseup.net>
2021-03-06 04:51:08 +0100

    gulpfile: do not minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const browserSync = require('browser-sync').create();
 function css() {
     return src('./src/*.scss', { sourcemaps: true })
         .pipe(sass())
-        .pipe(minifyCSS())
+        //.pipe(minifyCSS())
         .pipe(dest('./'), { sourcemaps: true })
         .pipe(browserSync.stream());
 }
